05d877fadca9fab3d2ba4fc2a23dbf7cc5a9554f
pablohayden <paulie.hayden@gmail.com>
2018-03-07 11:22:36 +0000

    removed extra shopping car sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,9 @@
-insert into product(id, product_name, unit_price) values (1, 'apple', '0.50')
-insert into product(id, product_name, unit_price) values (2, 'soup', '2.80')
-insert into product(id, product_name, unit_price) values (3, 'bread', '1.60')
-insert into product(id, product_name, unit_price) values (4, 'potato', '1.50')
+insert into product(id, product_name, unit_price) values (1, 'Apples', '1.00')
+insert into product(id, product_name, unit_price) values (2, 'Soup', '0.65')
+insert into product(id, product_name, unit_price) values (3, 'Bread', '0.80')
+insert into product(id, product_name, unit_price) values (4, 'Milk', '1.30')
 
-insert into shopping_cart(id, cart_name) values (1, 'pablo')
-insert into shopping_cart(id, cart_name) values (2, 'oisin')
-insert into shopping_cart(id, cart_name) values (3, 'aoife')
+
 
 
 
